7bc2d29f922839ec373d12355401c4d7763e0e08
Tcharls <charly.vanesse@gmail.com>
2023-09-20 10:22:20 +0100

    add timer

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 function initJs(color) {
+    // console.time('fnInitJs')
   console.log(arguments);
   console.warn(arguments);
   console.error(arguments);
   console.trace(arguments);
-  
+
   var jsLoadedNode = document.querySelector("#is-js-loaded");
   jsLoadedNode.innerHTML = "JS est bien <b>CHARGE</b>";
   jsLoadedNode.style.color = color;
   jsLoadedNode.style.textAlign = "center";
+//   console.timeEnd('fnInitJs')
 }
 
 initJs("black", "teste");
