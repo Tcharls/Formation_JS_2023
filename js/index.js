2895cd9b5df8b20ba2d56578dca01c6587c2eb8b
Tcharls <charly.vanesse@gmail.com>
2023-09-20 10:07:14 +0100

    creation fonction

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-var jsLoadedNode=document.querySelector('#is-js-loaded')
-jsLoadedNode.innerHTML="JS est bien <b>CHARGE</b>"
-jsLoadedNode.style.color="black"
-jsLoadedNode.style.textAlign="center"
+function initJs() {
+  var jsLoadedNode = document.querySelector("#is-js-loaded");
+  jsLoadedNode.innerHTML = "JS est bien <b>CHARGE</b>";
+  jsLoadedNode.style.color = "black";
+  jsLoadedNode.style.textAlign = "center";
+}
+
+initJs();
