924e09f04a7343d58b2ebcbc2d4481a53c5bad83
Tcharls <charly.vanesse@gmail.com>
2023-09-20 16:27:25 +0100

    ajout construction avec ou sans JSON

--- js/Meme.js
+++ js/Meme.js
@@ -3,8 +3,11 @@
  * variable commencant par "_" => privé (comme toute var déclarée)
  * "this." => public
  */
-
-function Meme(configuredMeme) {
+/**
+ * construction d'un meme avec ou sans JSON
+ * @param {string} jsonConfiguredMemeStr 
+ */
+function Meme(jsonConfiguredMemeStr) {
     this.texte = "";
     // this.id = "id du meme";
     this.posx = 0;
@@ -17,5 +20,14 @@ function Meme(configuredMeme) {
     this.color = "#000000";
     this.imageId = -1;
 
+    /**
+     * chargement valeurs depuis un meme en json
+     * @param {string} jsonStr 
+     */
+    this.loadFromString = function (jsonStr){
+            Object.assign(this,JSON.parse(jsonStr));
+        };
+
     }
+
 var meme=new Meme();
